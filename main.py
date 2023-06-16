1bae53df01f43a9f7390dc62a82bbe4cd5008e82
Kiskadee <Kiskadee.dev@gmail.com>
2023-06-15 22:53:38 -0300

    Implementado todas as funções do projeto

--- main.py
+++ main.py
@@ -1,14 +1,31 @@
+from datetime import datetime
+from enum import Enum
+
+
+class Operacoes(Enum):
+    DEPOSITO = "Depósito"
+    SAQUE = "Saque"
+
+
+class Operacao:
+    def __init__(self, valor: float, tipo: str):
+        self.valor = valor
+        self.tipo = tipo
+        self.data = datetime.now()
+
+
 class Bank:
     def __init__(self):
         self.saldo = 0
         self.limite = 500
-        self.extrato = "==EXTRATO=="
+        self.extrato = "====== EXTRATO ======"
+        self.historico: list[Operacao] = []
         self.numero_saques = 0
         self.LIMITE_SAQUES = 3
 
     def depositar(self, num: float) -> None:
         self.saldo += num
-        self.extrato += f"\nDEPÓSITO: R$ {num:.2f}"
+        self.historico.append(Operacao(num, Operacoes.DEPOSITO.value))
 
     def saque(self, num: float) -> None:
         """
@@ -18,7 +35,7 @@ class Bank:
         """
         excedeu_saldo = num > self.saldo
         excedeu_limite = num > self.limite
-        excedeu_numero_saques = self.numero_saques > self.LIMITE_SAQUES
+        excedeu_numero_saques = self.numero_saques > self.LIMITE_SAQUES - 1
 
         if excedeu_saldo:
             print("Falha! Saldo insuficiente para saque.")
@@ -26,13 +43,27 @@ class Bank:
             print("Falha! Limite excedido!")
         elif excedeu_numero_saques:
             print("Falha! Número de saques excedido!")
+        elif num <= 0:
+            print("Falha! Valor de saque inválido!")
         else:
             self.saldo -= num
-            self.extrato += f"\nSAQUE: R$ {num:.2f}"
             self.numero_saques += 1
+            self.historico.append(Operacao(num, Operacoes.SAQUE.value))
 
     def exibir_extrato(self) -> None:
+        """
+        Exibe o extrato
+        :return:
+        """
         print(self.extrato)
+        print(f"Data: {datetime.now()}")
+        print(f"Saldo atual: {self.saldo:.2f}")
+        if len(self.historico) > 0:
+            for op in self.historico:
+                print(f"{op.tipo}: R$ {op.valor:.2f} em {op.data}")
+        else:
+            print("Nenhuma transação efetuada.")
+        print("=====================")
 
 
 menu = """
@@ -41,7 +72,7 @@ menu = """
     [e] Extrato
     [d] Sair
 
-==>"""
+==> """
 
 
 def user_input(msg: str) -> float:
@@ -52,6 +83,7 @@ def user_input(msg: str) -> float:
     """
     num = input(msg)
     while not num.isnumeric():
+        print("Valor inválido!")
         num = input(msg)
     num = abs(float(num))
     return float(num)
